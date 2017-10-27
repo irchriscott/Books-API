54f62a833d1951e73b09043d7170127531e6d994
Christian Lurhakwa <irchristianscott@gmail.com>
2017-10-27 08:12:59 +0300

    changes

--- bookApi/settings.py
+++ bookApi/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'th^&%=t_o-+hu31d(au@xa%dqka8p8tn2r+x0a34dzo9u9=awq'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['198.211.99.20', 'localhost', '127.0.0.1']
 
 
 # Application definition
--- bookApi/urls.py
+++ bookApi/urls.py
@@ -4,6 +4,7 @@ from books import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+	url(r'/$', views.api_get_all_books, name="all_books_api_home" ),
 	url(r'api/books/all/$', views.api_get_all_books, name="all_books_api" ),
 	url(r'api/books/book=(?P<book_id>\d+)', views.api_get_single_book, name="single_book_api"),
 	url(r'api/books/search/term=(?P<term>[^/]+)&table=(?P<table>[^/]+)', views.api_search_book, name="search_book_api")
